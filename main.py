a226c5c13ebb3d5ebd4e3191528c2d5c24c0d62e
supreman <supreman2@gmail.com>
2019-04-26 00:09:36 +0300

    алгоритм ускорен и теперь работает для комнаты номер "2 млрд" примерно за полсекунды.

--- main.py
+++ main.py
@@ -5,29 +5,35 @@ def main( room ): # room - номер комнаты, которую ищем
     floor = 0 # номер этажа, на котором находится комната
     order = 0 # порядковый номер комнаты на этаже
 
-    cur_room = 0 # номер текущей просматриваемой комнаты
+    cur_room = 0 # номер последней просмотренной комнаты
 
     side = 0 # содержит сторону текущего квадрата 1*1, 2*2, 3*3 и так далее
+
     while True:
         side += 1
 
-        # TODO: ускорить алгоритм
-        # Дим, для больших номеров команты этот алгоритм сильно тормозит.
-        # поэтому тут есть, что доделать:
-        # для ускорения в этом месте можно добавлять к текущей комнате квадрат размера, и если
-        # полученная сумма меньше искомого номера команты, сразу прыгать на другой side
+        if cur_room + side**2 < room:
+            cur_room += side**2
+            floor += side
+            continue
 
-        for i in range(1, side):
+        for i in range(0, side):
             floor += 1
             order = 0
-            for j in range (1, side):
-                cur_room += 1
-                order += 1
-                if cur_room == room:
-                    return floor, order
+
+            if cur_room + side < room:
+                cur_room += side
+                continue
+
+            order += room - cur_room
+
+            return floor, order
 
 if __name__ == "__main__":
-    print("какую комнату будем искать: ")
-    room = int(input())
-    floor, order = main(room)
-    print( "комната с номером {0} находится на этаже {1}, порядковый номер комнаты на этаже {2}".format(room, floor, order) )
+    while True:
+        try:
+            room = int(input("какую комнату будем искать: "))
+        except ValueError: # чтобы выйти, достаточно ввести любое не_число
+            exit()
+        floor, order = main(room)
+        print( "комната с номером {0} находится на этаже {1}, порядковый номер комнаты на этаже {2}".format(room, floor, order) )
