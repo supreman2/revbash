703038af1020705b7c882ed107ca9265c3c43cef
supreman <supreman2@gmail.com>
2019-05-14 01:49:49 +0300

    ускорено

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+import main
+
+class TestFindRoom(unittest.TestCase):
+
+    def test_findroom(self):
+        self.assertEqual(main.main(1), (1,1))
+        self.assertEqual(main.main(4), (3, 1))
+        self.assertEqual(main.main(10), (5, 2))
+
+if __name__ == '__main__':
+    unittest.main()
